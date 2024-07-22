3c3b9859b0ee0c59ddaac965d72307f4e5948497
tobiasuihlein <tobias.uihlein@gmail.com>
2024-07-22 23:01:00 +0200

    create alternative layout

--- app.py
+++ app.py
@@ -22,48 +22,44 @@ html_code = f"""
 components.html(html_code, height=60)
 
 
-# Create widgets to specify grid properties by user input
-with st.container():
-    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
-
-    with col2: rows = st.number_input(label="Number of rows", min_value=1, max_value=10, value=4, step=1)
-    with col2: transition_size = st.slider(label="Transition size", min_value=0.1, max_value=5.0, value=2.5, step=0.1)
-
-    with col3: cols = st.number_input(label="Number of columns", min_value=1, max_value=10, value=6, step=1)
-    with col3: transition_speed = st.slider(label="Transition speed", min_value=0.1, max_value=5.0, value=2.5, step=0.1)
-    transition_speed = 1 / transition_speed
-
 # Create widget for image upload
 with st.container():
-    col1, col2, col3 = st.columns([1, 2, 1])
+    col1, col2, col3 = st.columns([1, 3, 1])
+    with col2: uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-with col2:
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Set grid properties if no user input
-if rows is None:
-    rows = 4
-    cols = 6
-    transition_size = 2.5
-    transition_speed = 1 / 2.5
-
-# Disply image in grid
+# Create widgets to specify grid properties by user input
 with st.container():
+    col1, col2, col3, col4, col5 = st.columns([1, 3, 0.05, 0.65, 0.3])
 
-    if uploaded_image is None:
-        url = "https://images.unsplash.com/photo-1573147367786-a5a227916f0c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        img_bytes, img_type = fetch_image_from_url(url)
-        bytes_data = img_bytes.read()
-        
-    else:
-        bytes_data = uploaded_image.read()
-        img_type = uploaded_image.name.split('.')[1]
+    with col4: rows = st.number_input(label="Number of rows", min_value=1, max_value=10, value=4, step=1)
+    with col4: cols = st.number_input(label="Number of columns", min_value=1, max_value=10, value=6, step=1)
+    with col4: transition_size = st.slider(label="Transition size", min_value=0.1, max_value=5.0, value=2.5, step=0.1)
+    with col4: transition_speed = st.slider(label="Transition speed", min_value=0.1, max_value=5.0, value=2.5, step=0.1)
+    transition_speed = 1 / transition_speed
 
-    # Convert bytes to image
-    img = Image.open(io.BytesIO(bytes_data))
-    img_width, img_height = img.size
+    with col2:
+        # Set grid properties if no user input
+        if rows is None:
+            rows = 4
+            cols = 6
+            transition_size = 2.5
+            transition_speed = 1 / 2.5
 
-    html_output = split_image_and_create_html_output(img, img_type, rows, cols, transition_speed, transition_size)
+        # Disply image in grid
+        if uploaded_image is None:
+            url = "https://images.unsplash.com/photo-1573147367786-a5a227916f0c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            img_bytes, img_type = fetch_image_from_url(url)
+            bytes_data = img_bytes.read()
+            
+        else:
+            bytes_data = uploaded_image.read()
+            img_type = uploaded_image.name.split('.')[1]
 
-    components.html("", height=30)
-    components.html(html_output, height=600)
+        # Convert bytes to image
+        img = Image.open(io.BytesIO(bytes_data))
+        img_width, img_height = img.size
+
+        html_output = split_image_and_create_html_output(img, img_type, rows, cols, transition_speed, transition_size)
+
+        components.html(html_output, height=600)
